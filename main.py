d8482b287146bcb74934659ab18be73f326bf8d4
Chandrakanth0698 <chandrakanth0698@gmail.com>
2023-11-06 17:20:23 -0500

    Return station name and temperature for any date

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
+df_meta = pd.read_csv("data_small/stations.txt", skiprows=17)
+
+
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 25
-    return {"station": station,
+    station_name = df_meta.loc[df_meta["STAID"] == int(station)]\
+    ["STANAME                                 "].squeeze()
+
+    df = pd.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt",
+                     skiprows=20,parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze()/10
+    return {"station": station_name.strip(),
+            "station_number": station,
             "date": date,
             "temperature": temperature}
 
